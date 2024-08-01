cfcdffa8a4747d2cce69940fc85eca4da50bcee7
fostemi <michael.isaac.foster@gmail.com>
2024-07-31 20:30:16 -0500

    begin data structure for workouts

--- src/read_workouts.py
+++ src/read_workouts.py
@@ -8,10 +8,21 @@ from read_google_spreadsheet import read_google_sheet
 WAKE_UP = '6:00:00 AM'
 WORKOUT_RANGE = "Sheet2!A:Z"
 
+class WorkoutProgram:
+    def __init__(self, workouts):
+        workouts = workouts
+    def add_workout(workout):
+        workouts += workout
+
+class Workout:
+    def __init__(self, day, week, excercise, distance):
+        self.day = day
+        self.week = week
+        self.excercise = excercise
+        self.distance = distance
+
 def parse_workout_sheet_title():
     values = read_google_sheet(WORKOUT_RANGE)
-    return values[0]
-    
-
+    return values[0][0]
 
 # We are reading the same spreadsheet so we just have to refactor the existing code to extract the read_sheet() function to it's own file
